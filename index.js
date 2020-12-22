92283c101c888f93f8f932ea716efe3946665729
SkyDonald <72652690+SkyDonald@users.noreply.github.com>
2020-12-22 18:56:07 +0100

    Init Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+const listener = server.listen(3000, function () {
+  console.log('App is ready');
+});
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const DBL = require('dblapi.js');
+const dbl = new DBL(process.env.DBLToken, { webhookServer: listener, webhookAuth: process.env.webhookPassword }, client);
+
+dbl.webhook.on('ready', function (hook) {
+  console.log('DBL Webhook is ready');
+});
+
+client.on('ready', function () {
+  console.log('Discord Client is ready');
+});
+
+dbl.webhook.on('vote', function (vote) {
+  const user = client.users.cache.get(vote.user);
+  if (!user) return;
+
+  user.send('Thanks for voting !');
+});
+
+client.login(process.env.discordToken);
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Top.gg vote scraper
+
+A simple Top.gg vote scraper made with the dblapi.js
+
+## Instructions
+
+### Downloading
+
+-   Download the source or clone the repo with `git clone https://github.com/SkyDonald/easy-dbl-vote`.
+-   Install [`NodeJS`](https://nodejs.org).
+-   After installing [`NodeJS`](https://nodejs.org) run `npm i` in the terminal to install the dependencies.
+-   Rename `dotenv` to `.env` and change `DBLToken` to your [DBL Token](https://top.gg/bot/764418734747549696/webhooks), `webhookPassword` to your webhook passwork and `discordToken` to your Discord bot token.
+    ![here](https://i.imgur.com/89hwJlH.png)
+-   After this all you can run `npm start`.
+
+## Any problems?
+Contact me on Discord `SkyDonald#2666`
